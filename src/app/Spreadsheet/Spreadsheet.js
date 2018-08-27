07409258bb6ad8af9521eaf2f52f5faea584c606
Nikita Savchenko <zitros.lab@gmail.com>
2018-08-27 19:53:41 +0300

    Add backup link, fix icon color

--- src/app/Spreadsheet/Spreadsheet.js
+++ src/app/Spreadsheet/Spreadsheet.js
@@ -36,6 +36,11 @@ export default class Spreadsheet extends Component {
         });
     }
 
+    @autobind
+    async onDownloadClick () {
+        window.open(`https://docs.google.com/spreadsheets/d/${ this.props.id }/export?format=xlsx&id=${ this.props.id }`);
+    }
+
     render () {
         const spreadsheet = this.state.spreadsheet;
         return <div class="spreadsheet">
@@ -43,6 +48,7 @@ export default class Spreadsheet extends Component {
                 <Icon size="big" image="back" clickable onClick={ this.onBackClick }/>
                 <EncryptionControl/>
                 <span class="title">{ spreadsheet ? spreadsheet.properties.title : "Loading..." }</span>
+                <Icon size="big" image="download" clickable onClick={ this.onDownloadClick }/>
             </div>
             <div class="body">{ !spreadsheet ? null :
                 <Sheet sheet={ spreadsheet.sheets[this.state.selectedSheet] }
